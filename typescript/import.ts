37cafb5714c88e6789c7010d81973c2c8c8d46f7
luka <luka.kilasonia.1@btu.edu.ge>
2021-11-01 07:39:03 +0400

    creat imports.ts file

---
+++ typescript/import.ts
@@ -0,0 +1,23 @@
+export interface validation {
+  index: number;
+  meta: {
+    title: string;
+    description: string;
+  };
+  call(store: any): any;
+  restore?(store: any): any;
+}
+
+export interface logs extends Omit<validation, "call"> {
+  storeBefore: {};
+  storeAfter?: {};
+  error: null | {
+    name: string,
+    message: string,
+    info: string
+  } | string;
+}
+
+
+
+
